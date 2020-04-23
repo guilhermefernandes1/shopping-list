3c1973bfcac5ab35bb189727d89ca5522aa731ff
Guilherme Anjos <guilhermefernandes1@gmail.com>
2020-04-22 21:10:50 -0300

    Created recipe list html and a test.

--- shopping-list/src/app/recipes/recipe-list/recipe-list.component.ts
+++ shopping-list/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,7 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeListComponent implements OnInit {
 
-  recipes = [];
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is a simply test.', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg'),
+    new Recipe('Hamburger Test', 'This is a hamburger test.', 'https://pixnio.com/free-images/2017/07/27/2017-07-27-15-31-07-900x600.jpg')
+  ];
 
   constructor() { }
 
